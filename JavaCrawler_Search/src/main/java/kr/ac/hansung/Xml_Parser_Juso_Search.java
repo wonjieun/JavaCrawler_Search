3c9d9ee9b91a282e349a3de2933ef8fb26edfd78
jieun <jieun@jieun>
2017-11-21 18:58:15 +0900

    Food_Region_Apt 엑셀파일 불러와 검색어(data)에 aptName열의 값들을 입력 후, 검색 결과 출력.

--- JavaCrawler_Search/src/main/java/kr/ac/hansung/Xml_Parser_Juso_Search.java
+++ JavaCrawler_Search/src/main/java/kr/ac/hansung/Xml_Parser_Juso_Search.java
@@ -1,16 +1,20 @@
 package kr.ac.hansung;
 
 import java.io.BufferedInputStream;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.net.URL;
 import java.net.URLEncoder;
 import java.util.ArrayList;
 
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
 import org.xmlpull.v1.XmlPullParser;
 import org.xmlpull.v1.XmlPullParserFactory;
 
 public class Xml_Parser_Juso_Search implements Xml_Parser {
-
 	public Xml_Parser_Juso_Search() {
 		try {
 			System.out.println("Xml_Parser_Juso_Search is running Now...");
@@ -21,83 +25,75 @@ public class Xml_Parser_Juso_Search implements Xml_Parser {
 	}
 
 	public void apiParserSearch() throws Exception {
-		URL url = new URL(getURLParam("청구빌라"));
-		XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
-		factory.setNamespaceAware(true);
-		XmlPullParser xpp = factory.newPullParser();
-		
-		BufferedInputStream bis = new BufferedInputStream(url.openStream());
-		xpp.setInput(bis, "utf-8");
 
-		String tag = null;
-		int event_type = xpp.getEventType();
-
+		FileInputStream fis = new FileInputStream("/Users/jieun/IdeaProjects/JavaCrawler/data/Food_Region_Apt.xls");
+		Workbook wbk = new HSSFWorkbook(fis);
+		Sheet sheet = wbk.getSheetAt(0);
+		int rowNum = sheet.getPhysicalNumberOfRows();
 		ArrayList<DTO> list = new ArrayList<DTO>();
 
-		String jibunAddr = null, siNm = null, sggNm = null, emdNm = null;
+		for (int i = 1; i < rowNum; i++) {
+			Row row = sheet.getRow(i);
 
-		while (event_type != xpp.END_DOCUMENT) {
-			
-			if (event_type == xpp.START_TAG) {
-				tag = xpp.getName();
-				System.out.println("Start tag = " + tag);
-				
-			} else if (event_type == xpp.TEXT) {
-				
-				if (tag.equals("totalCount")) {
-					System.out.println("totalCount=" + xpp.getText());
-				}
-				if (tag.equals("jibunAddr")) {
-					jibunAddr = xpp.getText();
-				} else if (tag.equals("siNm")) {
-					siNm = xpp.getText();
-				} else if (tag.equals("sggNm")) {
-					sggNm = xpp.getText();
-				} else if (tag.equals("emdNm")) {
-					emdNm = xpp.getText();
-				}
-				
-			} else if (event_type == xpp.END_TAG) {
-				tag = xpp.getName();
-				System.out.println("End tag = " + tag);
+			URL url = new URL(getURLParam(row.getCell(3).getStringCellValue()));
+			XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
+			factory.setNamespaceAware(true);
+			XmlPullParser xpp = factory.newPullParser();
 
-				if (tag.equals("juso")) {
-					// DTO 객체를 만들어 여기에 데이터를 집어넣어준다.
-					DTO entity = new DTO();
-					
-					entity.setJibunAddr(jibunAddr);
-					entity.setSiNm(siNm);
-					entity.setSggNm(sggNm);
-					entity.setEmdNm(emdNm);
-					
-					list.add(entity);
+			BufferedInputStream bis = new BufferedInputStream(url.openStream());
+			xpp.setInput(bis, "utf-8");
+
+			String tag = null;
+			int event_type = xpp.getEventType();
+
+			String jibunAddr = null, siNm = null, sggNm = null, emdNm = null;
+
+			while (event_type != xpp.END_DOCUMENT) {
+
+				if (event_type == xpp.START_TAG) {
+					tag = xpp.getName();
+
+				} else if (event_type == xpp.TEXT) {
+
+					if (tag.equals("jibunAddr")) {
+						jibunAddr = xpp.getText();
+					} else if (tag.equals("siNm")) {
+						siNm = xpp.getText();
+					} else if (tag.equals("sggNm")) {
+						sggNm = xpp.getText();
+					} else if (tag.equals("emdNm")) {
+						emdNm = xpp.getText();
+					}
+
+				} else if (event_type == xpp.END_TAG) {
+					tag = xpp.getName();
+
+					if (tag.equals("juso")) {
+						
+						if (sggNm.equals(row.getCell(2).getStringCellValue())) {
+							// DTO 객체를 만들어 여기에 데이터를 집어넣어준다.
+							DTO entity = new DTO();
+
+							entity.setJibunAddr(jibunAddr);
+							entity.setSiNm(siNm);
+							entity.setSggNm(sggNm);
+							entity.setEmdNm(emdNm);
+
+							list.add(entity);
+						}
+					}
+
 				}
-				
+				event_type = xpp.next();
 			}
-			event_type = xpp.next();
 		}
-
 		// 엑셀파일을 쓰는 부분
 		new Excel_Writer_Xml_Juso_Search().makeExcelFileParmList(list);
-
+		fis.close();
 	}
-
-	/*
-	 * public ArrayList<DTO> returnList(int year) throws Exception {
-	 * FileInputStream fis = new FileInputStream(
-	 * "C:\\Users\\admin\\IdeaProjects\\JavaCrawler\\data\\Food_Region_Apt.xls")
-	 * ; Workbook wbk = new HSSFWorkbook(fis); Sheet sheet = wbk.getSheetAt(0);
-	 * int rowNum = sheet.getPhysicalNumberOfRows();
-	 * 
-	 * Row row = sheet.getRow(i); ArrayList<DTO> list = new ArrayList<DTO>();
-	 * 
-	 * String jibunAddr = null, siNm = null, sggNm = null, emdNm = null; }
-	 */
+	
 	public String getURLParam(String data) throws IOException {
-		String url = PARAM_URL + ROF + APT_NAME + URLEncoder.encode(data,"UTF-8") + KEY;
-		/*
-		 * if(search != null){ url = url+"&yadmNm"+search; }
-		 */
+		String url = PARAM_URL + ROF + APT_NAME + URLEncoder.encode(data, "UTF-8") + KEY;
 		return url;
 	}
 
